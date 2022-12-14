56d1ff9f469aa60494fcab1011e8a1917c22b2c4
lera <lera_bal@list.ru>
2022-12-14 14:55:28 +0300

    IfElse

---
+++ Example005_If_Else/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if (username.ToLower()=="лера")
+{
+   Console.WriteLine ("Да этож Лерка!!!!!"); 
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
