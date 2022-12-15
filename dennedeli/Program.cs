521a8f4ed8a511b20a24713380c5f18ed07bf9ec
lera <lera_bal@list.ru>
2022-12-15 22:09:37 +0300

    Семинар

---
+++ dennedeli/Program.cs
@@ -0,0 +1,39 @@
+﻿// Дни недели3. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+
+Console.Clear();
+
+Console.WriteLine ("Введите номер дня недели:" );
+int userday = int.Parse(Console.ReadLine ()!);
+
+if (userday==1)
+{
+    Console.Write("Понедельник ");
+}
+else if (userday==2)
+{
+    Console.Write("Вторник ");
+}
+else if (userday==3)
+{
+    Console.Write("Среда ");
+}
+else if (userday==4)
+{
+    Console.Write("Четверг ");
+}
+else if (userday==5)
+{
+    Console.Write("Пятница ");
+}
+else if (userday==6)
+{
+    Console.Write("Суббота ");
+}
+else if (userday==7)
+{
+    Console.Write("Воскресенье ");
+}
+else 
+{
+    Console.WriteLine ("Введите число от 1 до 7" );
+    }
---
+++ seminarus15/Program.cs
@@ -0,0 +1,8 @@
+﻿//Задача 0 Напишите программу которая на вход принимает число и выведет его квадрат
+
+Console.Clear();
+Console.Write ("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+int sqr = number * number;
+Console.WriteLine($"Квадрат числа {number} равен {sqr}");
+
