442950a4cf5e19f957c751acc5cbe4be7ac14be0
lera <lera_bal@list.ru>
2022-12-17 13:35:48 +0300

    MetodArray

---
+++ Example010_MetodArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = {25,59,78,4,458,9,5,7,0};
+
+int n = array.Length; //автоматически определяет длину массива int[] array = {25,59,78,456,458,9,5,2,0}; ВЗОВРАЩАЕТ ДЛИНУ (КОЛ-ВО ЭЛЕМЕНТОВ) МАССИВА
+
+int find = 458;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine (index); // break; - прерви
+    }
+    index++;
+}
