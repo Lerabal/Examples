3ae5adc53c0a27d9e1817efa603caacae9001fce
lera <lera_bal@list.ru>
2022-12-20 11:48:15 +0300

    zadacha 14 kratno

---
+++ zadacha13_kratno3_7/Program.cs
@@ -0,0 +1,16 @@
+﻿/*14. Принимает на вход число и проверяет кратно ли оно 7 и 23 одновременно*/
+
+Console.Clear();
+Console.Write ("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+int a = 7;
+int b = 3;
+
+if (number % a ==0 && number % b == 0)
+{
+    Console.WriteLine("Кратно обоим числам");
+}
+else
+{
+    Console.WriteLine("Число не кратно обоим числам");
+}
