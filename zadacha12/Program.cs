6740a46e30a42b7bee95b3f394071b263d1a4af0
lera <lera_bal@list.ru>
2022-12-20 12:13:46 +0300

    Zadacha12

--- zadacha12/Program.cs
+++ zadacha12/Program.cs
@@ -3,12 +3,13 @@ Console.Write("Введите 1 число ");
 int num = int.Parse(Console.ReadLine()!);
 Console.Write("Введите 2 число ");
 int num1 = int.Parse(Console.ReadLine()!);
-if (num%num1 == 0)
+if (num % num1 == 0)
 {
     Console.Write("Число 1 кратно число 2");
 }
 else
 {
-    int ost = num%num1;
+    int ost = num % num1;
     Console.Write($"Остаток от деления {ost}");
 }
+/*Принимает на вход 2 числа и выводит, я вляется ли второе число кратно первому, и если нет то выводит остаток
