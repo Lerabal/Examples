48642fe0b98d5bb09878e3e629cf327127fc46b1
lera <lera_bal@list.ru>
2022-12-20 12:12:12 +0300

    Zadacha12

---
+++ zadacha12/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите 1 число ");
+int num = int.Parse(Console.ReadLine()!);
+Console.Write("Введите 2 число ");
+int num1 = int.Parse(Console.ReadLine()!);
+if (num%num1 == 0)
+{
+    Console.Write("Число 1 кратно число 2");
+}
+else
+{
+    int ost = num%num1;
+    Console.Write($"Остаток от деления {ost}");
+}
