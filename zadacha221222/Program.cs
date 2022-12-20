8fcf0a960d1d60d4569f48482bc86f2554b7848c
lera <lera_bal@list.ru>
2022-12-20 14:31:45 +0300

    Palindr

---
+++ zadacha221222/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.Write("Введите пятизначное число:   ");
+string number = Console.ReadLine()!;
+int length = number.Length;
+string txtToCheck = number.Replace(" ","");  // для проверки текста на палиндром
+
+bool Palindrome()
+{
+    for (int i = 0; i < length / 2; i++)
+        while (number[i] == number[length - 1])
+        {
+            return true;
+        }
+    return false;
+}
+
+if (Palindrome()) Console.WriteLine($"{number} - является палиндромом");
+else Console.WriteLine($"{number} не является палиндромом");
+
+
