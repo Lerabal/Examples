4dc4e641b83b47b701750c7120f6c12a9d770f9e
lera <lera_bal@list.ru>
2022-12-22 21:17:27 +0300

    zadacha22

---
+++ zadacha22/Program.cs
@@ -0,0 +1,32 @@
+﻿/* Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел 
+от 1 до N.
+
+5 -> 1, 4, 9, 16, 25.
+2 -> 1,4*/
+
+Console.Clear();
+int getUserValue(string message)
+{
+    Console.WriteLine(message);
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
+}
+
+int n = getUserValue("Введите n");
+
+for (int i = 1; i<n+1; i++)
+{
+    int temp = i*i;
+    Console.Write($"  {temp}");
+}
+
+
+/*int n2 = 1;
+
+while (n2 <= n)
+{
+   int cub=n2*n2*n2;
+   Console.Write(cub + " ");
+   n2++;
+}
+Console.WriteLine();*/
