f10f4b05c80b25062dc386618cfccaceaacca710
lera <lera_bal@list.ru>
2022-12-22 17:31:16 +0300

    00

--- fromN_n/Program.cs
+++ fromN_n/Program.cs
@@ -2,7 +2,7 @@
 //4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
 //2 -> " -2, -1, 0, 1, 2"
 
-Console.Clear();
+/*Console.Clear();
 Console.Write ("Введите число: ");
 int number1 = int.Parse(Console.ReadLine()!);
 int number2 = -number1;
@@ -11,4 +11,17 @@ while (number2 <= number1)
 {
    Console.Write(number2+ " ");
    number2++;
+}*/
+
+Console.Clear();
+Console.Write ("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+int n2 = 1;
+
+while (n2 <= n)
+{
+   int cub=n2*n2*n2;
+   Console.Write(cub + " ");
+   n2++;
 }
+Console.WriteLine();
